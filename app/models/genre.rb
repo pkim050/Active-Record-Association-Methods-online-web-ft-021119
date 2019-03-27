3c4f1a1c842103f6ea6c48e606a7808101b77bad
Patrick Kim <pkim050@gmail.com>
2019-03-27 01:52:48 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,8 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artists.all
+    arr = []
+    self.artists.all.each {|element| arr << element.name}
+    arr
   end
 end
